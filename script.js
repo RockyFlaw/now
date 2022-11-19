c855c879e088762310a4927cf9f7f6a90db08abd
RockyFlaw <115312067+RockyFlaw@users.noreply.github.com>
2022-11-19 21:26:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let i = 0;
+while (i < 3) { 
+  alert( i );
+  i++;
+}
+
+let userName = 'Байда';
+
+function showMessage() {
+  let message = 'Привіт, ' + userName;
+  alert(message);
+}
+
+showMessage();
